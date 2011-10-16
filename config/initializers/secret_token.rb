7525be92eaefe068849274db45d322a58b3948ea
Chris Nelson <superchrisnelson@gmail.com>
2011-10-15 23:29:26 -0400

    Initial checkin with jasminerice added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = '8dfd52ca97dd0dd5593454b0dbac50c146975c4fea5dd878a0d6afc793e37aefe2d4c2467898e9ee64e5690b61d3a2da27a3805d5b8bcac5268893579c1f6653'
